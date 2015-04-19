dea5c3e577ce916108d1de2f2885af4682660c7e
JustBeYou <mihailferaru2000@gmail.com>
2015-04-19 14:38:50 +0300

    Declarations of database functions.

--- bank.h
+++ bank.h
@@ -46,6 +46,50 @@ typedef struct bank {
 	int self_funds;          // The funds of bank without accounts money
 	int accounts_money;      // The money of all accounts
 	// TODO: add array of users when user struct is ready
-} bank_t; 
+} bank_t;
+
+/* Initialize bank 
+ *
+ * Alloc memeory for a 'bank_t' variable with specified name. After it search for
+ * database of bank, if it doesn't exist create one and generate a random key for
+ * it, else it reads data from database file and writes to the new allocated 'bank_t'
+ * variable. At last, it returns the 'bank_t' variable.
+ * 
+ * Parameters: name of bank (string)
+ * Return: bank variable (bank_t) - ERROR: return NULL
+ */
+bank_t *init_bank(char *name);
+
+/* Load bank database
+ * 
+ * Search for a database file with specified name (bank->name) and load it in memory,
+ * in the bank structure. If the database wasn't found or any error occur in time of
+ * loading, the return value will be 1.
+ *
+ * Parameters: bank structure (bank_t)
+ * Return: 0 - SUCCESS, 1 - ERROR
+ */
+int load_db(bank_t *bank);
+
+/* Create bank database
+ *
+ * Create a database file with bank name (bank->name) and generates a key for database
+ * which will be loaded into bank key (bank->key). If any error occur, function will
+ * return 1.
+ *
+ * Parameters: bank structure (bank_t)
+ * Return: 0 - SUCCESS, 1 - ERROR
+ */
+int create_db(bank_t *bank);
+
+/* Calculate all bank funds
+ *
+ * Take a bank structure and calculate real money (bank->real_money), self funds (bank->self_funds)
+ * and accounts funds (bank->accounts_money). If any error occur return 1;
+ *
+ * Parameters: bank structure (bank_t)
+ * Return: 0 - SUCCESS, 1 - ERROR
+ */
+int calculate_funds(bank_t *bank);
 
 #endif // _BANK_H
